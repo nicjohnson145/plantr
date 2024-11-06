d73923f23bcad118d63b31ef6a66d77d8a97141a
Nic Johnson <nicjohnson145@hotmail.com>
2024-11-05 19:44:29 -0600

    feat: [WIP] checkpoint

--- internal/storage/sqlite-migrations/20241025033311_initial.down.sql
+++ internal/storage/sqlite-migrations/20241025033311_initial.down.sql
@@ -1,5 +1,7 @@
 BEGIN;
 
 DROP TABLE IF EXISTS challenge;
+DROP TABLE IF EXISTS github_release_version;
+
 
 COMMIT;
--- internal/storage/sqlite-migrations/20241025033311_initial.up.sql
+++ internal/storage/sqlite-migrations/20241025033311_initial.up.sql
@@ -5,4 +5,10 @@ CREATE TABLE challenge (
     value TEXT NOT NULL
 );
 
+CREATE TABLE github_release_version (
+    repo           TEXT NOT NULL PRIMARY KEY,
+    latest_version TEXT NOT NULL,
+    last_check     DATETIME
+);
+
 COMMIT;
