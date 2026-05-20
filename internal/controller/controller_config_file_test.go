65e9eef4987893898a4594f5827e5934a2ebb248
Nic Johnson <nicjohnson145@hotmail.com>
2026-05-20 15:26:54 -0500

    test(TDD): add unit test exposing #5

--- internal/controller/controller_config_file_test.go
+++ internal/controller/controller_config_file_test.go
@@ -2,12 +2,14 @@ package controller
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/lithammer/dedent"
 	"github.com/nicjohnson145/hlp"
 	pbv1 "github.com/nicjohnson145/plantr/gen/plantr/controller/v1"
 	"github.com/nicjohnson145/plantr/internal/parsingv2"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
@@ -28,7 +30,7 @@ func TestRenderSeedConfigFile(t *testing.T) {
 				},
 				Element: &parsingv2.ConfigFile{
 					TemplateContent: "seed-one-content",
-					Destination: "~/seed-one",
+					Destination:     "~/seed-one",
 				},
 			},
 			{
@@ -55,7 +57,7 @@ func TestRenderSeedConfigFile(t *testing.T) {
 				},
 				Element: &pbv1.Seed_ConfigFile{
 					ConfigFile: &pbv1.ConfigFile{
-						Content: "seed-one-content",
+						Content:     "seed-one-content",
 						Destination: "/tmp/someuser/seed-one",
 					},
 				},
@@ -66,12 +68,58 @@ func TestRenderSeedConfigFile(t *testing.T) {
 				},
 				Element: &pbv1.Seed_ConfigFile{
 					ConfigFile: &pbv1.ConfigFile{
-						Content: "\nseed-one-installed\n",
+						Content:     "\nseed-one-installed\n",
 						Destination: "/tmp/someuser/seed-two",
 					},
 				},
 			},
 		}
 		pbEqual(t, wantPb, pbSeeds)
+	})
+
+	t.Run("secret references change hash", func(t *testing.T) {
+		// Make an vault mock that returns different "secret data" on each call to simulate it being changed
+		count := 0
+		vault := NewMockVaultClient(t)
+		vault.
+			EXPECT().
+			ReadSecretData(mock.Anything).
+			RunAndReturn(func(ctx context.Context) (map[string]any, error) {
+				count += 1
+				return map[string]any{
+					"foo": fmt.Sprintf("bar-%v", count),
+				}, nil
+			})
+
+		ctrl, err := NewController(ControllerConfig{
+			VaultClient: vault,
+		})
+		require.NoError(t, err)
+
+		node := &parsingv2.Node{
+			UserHome: "/tmp/someuser",
+		}
+		seeds := []*parsingv2.Seed{
+			{
+				Metadata: &parsingv2.SeedMetadata{
+					Name: hlp.Ptr("seed-one"),
+				},
+				Element: &parsingv2.ConfigFile{
+					TemplateContent: "hello im {{ .Vault.foo }}",
+					Destination:     "~/seed-one",
+				},
+			},
+		}
+
+		firstPbSeeds, err := ctrl.renderSeeds(context.Background(), node, seeds)
+		require.NoError(t, err)
+		fmt.Println(firstPbSeeds[0].GetConfigFile().Content)
+
+		secondPbSeeds, err := ctrl.renderSeeds(context.Background(), node, seeds)
+		require.NoError(t, err)
+		fmt.Println(secondPbSeeds[0].GetConfigFile().Content)
+
+		// ensure that the hash for the config file is different between runs
+		require.NotEqual(t, firstPbSeeds[0].Metadata.Hash, secondPbSeeds[0].Metadata.Hash)
 	})
 }
